9e62db177cc8e347ff41832e8a4ed5a277327110
Yaroslav Vorobyov <yaroslav-vorobyov@yandex.ru>
2024-05-23 17:12:17 +0300

    update style crontab_tasks.sh

--- functions/crontab_tasks.sh
+++ functions/crontab_tasks.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 # 
 ## Color output for all cron tasks and all users
-
-# Set colors
-red='\e[0;31m'
-RED='\e[1;31m'
-green='\e[0;32m'
-GREEN='\e[1;32m'
-NC='\e[0m'
+# 
+# add to ~/.bash_aliases:
+# echo 'source ~/.scripts/functions/crontab_tasks.sh' >> ~/.bash_aliases
 
 # Output list of all cron tasks from all users
 function allcrontab() {
+    # Set colors
+    red='\e[0;31m'
+    RED='\e[1;31m'
+    green='\e[0;32m'
+    GREEN='\e[1;32m'
+    NC='\e[0m'
+
     for user in $(cut -d':' -f1 /etc/passwd); do
         usercrontab=$(crontab -l -u ${user} 2>/dev/null)
             if [ -n "${usercrontab}" ]; then
